d36ed02e5feca24d16b5d153cb1245df50dc100d
sanjay sah <er.sanjaysah@hotmail.com>
2024-04-02 23:05:01 +0530

    sended change file

--- s3_and_cdn.tf
+++ s3_and_cdn.tf
@@ -1,5 +1,5 @@
-# resource "aws_s3_bucket" "upload_file" {
-#   bucket = "prodios-labs-upload-bucket-0001"
+resource "aws_s3_bucket" "upload_file" {
+  bucket = "infinix-hot-acer-apple-bucket"
 #   policy = <<EOF
 # {
 #   "Version": "2012-10-17",
@@ -7,55 +7,55 @@
 #     {
 #       "Effect": "Allow",
 #       "Action": ["s3:GetObject", "s3:PutObject"],
-#       "Resource": "arn:aws:s3:::prodios-labs-upload-bucket-0001/*"
+#       "Resource": "arn:aws:s3:::testbucket/*"
 #     }
 #   ]
 # }
 # EOF
-# }
+}
 
-# resource "aws_s3_bucket_object" "example_object" {
-#   bucket = aws_s3_bucket.upload_file.id
-#   key    = "index.html"
-#   source = "F:/Prodios_Assignment/prodios_labs/index.html"
-# }
+resource "aws_s3_bucket_object" "example_object" {
+  bucket = aws_s3_bucket.upload_file.id
+  key    = "index.html"
+  source = "F:/Prodios_Assignment/prodios_labs/index.html"
+}
 
-# # Create a CloudFront distribution
-# resource "aws_cloudfront_distribution" "web_cdn" {
-#   origin {
-#     domain_name = aws_s3_bucket.upload_file.bucket_regional_domain_name
-#     origin_id   = "S3-${aws_s3_bucket.upload_file.id}"
-#   }
+# Create a CloudFront distribution
+resource "aws_cloudfront_distribution" "web_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.upload_file.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.upload_file.id}"
+  }
 
-#   enabled             = true
-#   is_ipv6_enabled     = false
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = false
+  default_root_object = "index.html"
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = "S3-${aws_s3_bucket.upload_file.id}"
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.upload_file.id}"
 
-#     forwarded_values {
-#       query_string = true
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 200
-#     max_ttl                = 1000
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 200
+    max_ttl                = 1000
+  }
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true # Used the CloudFront default certificate
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true # Used the CloudFront default certificate
+  }
+}
